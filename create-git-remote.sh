0c475dd8c19936355aaf1145872bd8822f322913
Sameer Reddy <Sameer@Sameers-MBP.attlocal.net>
2016-07-29 17:34:04 -0700

    default

--- create-git-remote.sh
+++ create-git-remote.sh
@@ -1,18 +1,7 @@
 #!/bin/bash -e
 
-# Create a directory with chosen name and a -
-# remote git repositiory for it
-
-# Make directory
-read -p "Enter Directory Name:" NEW_DIR
-mkdir $NEW_DIR
-cd $NEW_DIR
-
-# Setup git
-git init
-echo "# ${NEW_DIR}" >> README.md
-
 #Create remote Git repo from command line
+
 read -p "Enter Git username:" USER
 read -p "Enter repo name:" REPO
 
