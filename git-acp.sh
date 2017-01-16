ea1ecf6415ee49a793145a0b410701ecc4b47a5f
Sameer Reddy <Sameer@Sameers-MacBook-Pro.local>
2017-01-15 19:24:44 -0800

    a

--- git-acp.sh
+++ git-acp.sh
@@ -23,7 +23,7 @@ while getopts ":s" option; do
 done
 
 if [ "$SUPPRESS" = 0 ]; then
-    read -p "Enter commit message: " MESSAGE;
+    read -p "Enter commit essage: " MESSAGE;
     BRANCH=$2;
 fi
 
